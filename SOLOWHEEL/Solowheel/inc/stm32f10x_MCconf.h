1df95319ae5e9f64c8d0e4e67157f76582fc5368
JagjitSingh99 <147207221+JagjitSingh99@users.noreply.github.com>
2025-04-09 13:35:57 -0500

    Delete SOLOWHEEL directory

--- SOLOWHEEL/Solowheel/inc/stm32f10x_MCconf.h
+++
@@ -1,9 +0,0 @@
-
-/* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __STM32F10x_MCCONF_H
-#define __STM32F10x_MCCONF_H
-
-/* Uncomment to enable differential terms of PIDs */           
-//#define DIFFERENTIAL_TERM_ENABLED
-  
-#endif /* __STM32F10x_MCCONF_H */
